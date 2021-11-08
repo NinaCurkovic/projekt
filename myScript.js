e179fda804e0e9e476b98f4e16b2377e7abbf27c
Nina Curkovic <ncurkovic@pmfst.hr>
2021-11-08 12:24:02 +0100

    popravila search i kategoriju

--- myScript.js
+++ myScript.js
@@ -37,7 +37,7 @@ window.onload = function () {
 
 	function addHTMLContent(element, data) {
 		element.innerHTML += `
-			<div class="shopping-products" >
+			<div class="shopping-products" data-category=${data.genderType}>
 				<img src="${data.imgSource}" alt="${data.imgAlt}" class="img"><br>
 				<p style="text-align: center;">${data.description}</p>
 				<p style="text-align: center;">${data.price} KN</p>
@@ -54,34 +54,10 @@ window.onload = function () {
 		}
 	});
 
-	function addWomanProductElement(product) {
-		var womenSection = document.getElementById('women');
-		if (womenSection) {
-			addHTMLContent(womenSection, product);
-		}
-	}
-
-	function addManProductElement(product) {
-		var menSection = document.getElementById('men');
-		if (menSection) {
-			addHTMLContent(menSection, product);
-		}
-	}
-
-	function addChildProductElement(product) {
-		var childSection = document.getElementById('children');
-		if (childSection) {
-			addHTMLContent(childSection, product);
-		}
-	}
-
 	function addProductElement(product) {
-		if (product.genderType === 'women') {
-			addWomanProductElement(product);
-		} else if (product.genderType === 'man') {
-			addManProductElement(product);
-		} else {
-			addChildProductElement(product);
+		var section = document.getElementById('all_product');
+		if (section) {
+			addHTMLContent(section, product);
 		}
 	}
 
@@ -92,41 +68,32 @@ window.onload = function () {
 		let input = document.getElementById('searchbar').value
 		input=input.toLowerCase();
 		let x = document.getElementsByClassName('shopping-products');
-
-		for (i = 0; i < x.length; i++) { 
-			if (!x[i].innerHTML.toLowerCase().includes(input)) {
-				x[i].style.display="none";
+		//spread operator za dobivanje elemenata iz neke kolekcije
+		[...x].forEach(el => {
+			if (!el.innerHTML.toLowerCase().includes(input)) {
+				el.style.display="none";
 			}
 			else {
-				x[i].style.display="inline-block";                 
+				el.style.display="inline-block";                 
 			}
-		}
+		})
 	}
+	document.getElementById('searchbar').addEventListener("keyup", search)
 	// Filter Dropdown list
 	function filterDropdown() {
 		var user = document.getElementById("myDropdown").value;
-		var women = document.getElementById("women");
-		var men = document.getElementById("men");
-		var children = document.getElementById("children");
-		if(user=="women") {
-			women.style.display=" inline-block";
-			men.style.display="none";
-			children.style.display="none";
-		} else if (user=="men") {
-			women.style.display="none";
-			men.style.display=" inline-block";
-			children.style.display="none";
-		} else if (user=="children") {
-			women.style.display="none";
-			men.style.display="none";
-			children.style.display=" inline-block";
-		}
-		else if (user=="all") {
-			women.style.display="inline-block";
-			men.style.display="inline-block";
-			children.style.display="inline-block";
-		}
+		var products = document.getElementsByClassName('shopping-products');
+		console.log('a');
+		[...products].forEach(elem => {
+			if(user==elem.dataset.category || user=="all"){
+				elem.style.display='inline-block';
+			}
+			else{
+				elem.style.display='none';
+			}
+		})
 	}
+	document.getElementById('myDropdown').addEventListener("change", filterDropdown)
 	// e-mail validation
 	function validate_email(field_condition, alert_message) {
 		with(field_condition) {
